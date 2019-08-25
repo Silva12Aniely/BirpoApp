9f9a38e5186f6e2282e5dbf78d646b24b3924931
Aniely <anielysilva08@gmail.com>
2019-08-25 17:06:17 -0300

    25/08 - up

--- app/src/main/java/com/example/birpoapp/MainActivity.java
+++ app/src/main/java/com/example/birpoapp/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.birpoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button login;
+    EditText txtuser, txtpass;
+
+    public static final String EXTRA_MESSAGE = "BirpoApp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.btn_login);
+        txtuser = (EditText) findViewById(R.id.edt_Login);
+        txtpass = (EditText) findViewById(R.id.edt_senha);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user, pass;
+
+                user = txtuser.getText().toString();
+                pass = txtpass.getText().toString();
+
+                if (user.equals("admin") && pass.equals("password")) {
+                    Intent intent = new Intent(getApplicationContext(), Produtos_Activity.class);
+
+                    String message = txtuser.getText().toString();
+                    intent.putExtra(EXTRA_MESSAGE, message);
+
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Toast.makeText(getApplicationContext(), "Usuário e Senha Inválidos!", Toast.LENGTH_LONG).show();
+                    txtuser.setText("");
+                    txtpass.setText("");
+                    txtuser.requestFocus();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/birpoapp/Produtos_Activity.java
+++ app/src/main/java/com/example/birpoapp/Produtos_Activity.java
@@ -3,14 +3,21 @@ package com.example.birpoapp;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Produtos_Activity extends AppCompatActivity {
     Toolbar toolbar;
+    List<Produtos> lstProd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,20 @@ public class Produtos_Activity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbarprodutos);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        lstProd = new ArrayList<>();
+        lstProd.add(new Produtos(R.drawable.pnescafe, "Nescafé", "Marca de café", 10));
+        lstProd.add(new Produtos(R.drawable.banana, "Banana", "Fruta", 5));
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.idrecyclerView);
+        BirpoAdapterRecyclerView AdapterRecyclerView = new BirpoAdapterRecyclerView(getApplicationContext(), lstProd);
+        recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+        recyclerView.setAdapter(AdapterRecyclerView);
+
+       RecyclerView recyclerView1 = (RecyclerView) findViewById(R.id.idrecyclerViewtwo);
+       BirpoAdapterRecyclerView recyclerView2 = new BirpoAdapterRecyclerView(getApplicationContext(), lstProd);
+       recyclerView1.setLayoutManager(new GridLayoutManager(getApplicationContext(), 2));
+       recyclerView1.setAdapter(recyclerView2);
     }
 
     @Override
